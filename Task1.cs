ee91c1031c9875502177338f5313b880118924f7
AliceVei21 <alicevei21@gmail.com>
2023-01-08 21:41:23 +0100

    second

--- Task1.cs
+++ Task1.cs
@@ -10,25 +10,24 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-                 Console.Clear();
+            Console.Clear();
             Console.WriteLine("Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1.");
             int n = InputNumbers("Введите n: ");
             int x = 2;
-            PrintNumber(n, x);
+            PrintNumberToConsole(n, y);
             Console.Write(1);
 
             int InputNumbers(string input)
             {
                 Console.Write(input);
-                int output = Convert.ToInt32(Console.ReadLine());
-                return output;
+                int outt = Convert.ToInt32(Console.ReadLine());
+                return outt;
             }
-
-            void PrintNumber(int n1, int x2)
+            void PrintNumberToConsole(int n1, int y2)
             {
                 if (x2 > n1) return;
-                PrintNumber(n1, x2 + 1);
-                Console.Write(x2 + ", ");
+                PrintNumberToConsole(n1, y2 + 1);
+                Console.Write(y2 + ", ");
             }
         }
     }
